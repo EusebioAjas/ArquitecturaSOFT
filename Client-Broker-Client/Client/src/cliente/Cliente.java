5a24b749a7043de6c100e91b6e1782e07942fb30
EusebioAjas <eusebioajas6@gmail.com>
2020-12-30 14:45:42 -0600

    init commit broker with trivial socket example

---
+++ Client-Broker-Client/Client/src/cliente/Cliente.java
@@ -0,0 +1,16 @@
+package cliente;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Cliente {
+  private static final String IP_ADDRESS = "127.0.0.1";
+  private static final int PORT = 59090;
+
+  public static void main(String[] args) throws IOException {
+    var socket = new Socket(IP_ADDRESS, PORT);
+    var in = new Scanner(socket.getInputStream());
+    System.out.println("Server response: " + in.nextLine());
+  }
+}
---
+++ Client-Broker-Client/Server/src/server/Server.java
@@ -0,0 +1,21 @@
+package server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.util.Date;
+
+public class Server {
+  private static final int PORT = 59090;
+  public static void main(String[] args) throws IOException {
+    try(var listener = new ServerSocket(PORT)){
+      System.out.println("The date server is running...");
+      while (true){
+        try(var socket = listener.accept()){
+          var out = new PrintWriter(socket.getOutputStream(), true);
+          out.println(new Date().toString());
+        }
+      }
+    }
+  }
+}
