6eb30c205fd307cf556ce7a789b20869437979a5
EusebioAjas <eusebioajas6@gmail.com>
2021-01-06 18:38:09 -0600

    modificaciones a broker

--- Client-Broker-Client/src/Server/src/server/Server.java
+++ Client-Broker-Client/src/Server/src/server/Server.java
@@ -3,19 +3,30 @@ package server;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.util.Date;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
   private static final int PORT = 59090;
+  private static final String IP_ADDRESS = "127.0.0.1";
+
   public static void main(String[] args) throws IOException {
-    try(var listener = new ServerSocket(PORT)){
+    /*try(var listener = new ServerSocket(PORT)){
       System.out.println("The date server is running...");
       while (true){
         try(var socket = listener.accept()){
           var out = new PrintWriter(socket.getOutputStream(), true);
-          out.println(new Date().toString());
+          out.println();
         }
       }
-    }
+    }*/
+
+    var socket = new Socket(IP_ADDRESS, PORT);
+    var in = new Scanner(socket.getInputStream());
+    System.out.printf("Server response: " + in.nextLine());
+  }
+
+  private static void response() throws IOException {
+
   }
 }
